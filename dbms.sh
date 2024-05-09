36370dfbd0398d691df998ea9deb74999d23377f
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2024-05-10 00:30:23 +0300

    Add Create and List DBs

--- dbms.sh
+++ dbms.sh
@@ -3,16 +3,24 @@
 
 
 # Database Management Functions
-
+MainPath=$(pwd)
 dbms () {
     while true; do
     echo ""
     echo "DataBase Engine : "
-    echo "1. Create Database :"
+    echo "1. Create Database"
     echo "2. List Database"
     echo "3. connect to Database"
     echo "4. Drop Database"
     echo "5. Exit"
+    read choice
+    case $choice in
+        1) create_data_base;;
+        2) list_data_base;;
+        5) echo "Exiting..."; exit ;;
+        *) echo "Invalid choice";;
+        esac 
+
     done
 
 }
@@ -45,9 +53,50 @@ databaseMenu (){
         5) echo "select_from_table" ;;
         6) echo "delete_from_table" ;;
         7) echo "update_table" ;;
-        8) dbms;;
-        9) echo "Exiting..."; break ;;
+        8) cd $MainPath;dbms;;
+        9) echo "Exiting..."; exit ;;
         *) echo "Invalid choice";;
     esac
     done
 }
+
+create_data_base() 
+{
+    echo ""
+    echo "Enter database name: "
+    read database
+    #check if dir database found 
+    if [[ -d "DataBase" ]] > /dev/null ;then
+        cd ./DataBase
+        if [[ -d "$database" ]] > /dev/null ;then
+            echo "Data base are ready exit"
+            dbms
+        fi
+        mkdir $database
+        cd ./$database
+        echo "Data base is creating and Now your using $database "
+        databaseMenu $database
+        
+    else 
+        mkdir DataBase
+        cd ./DataBase
+        mkdir $database
+        cd ./$database
+        echo "Data base is creating and Now your using $database "
+        databaseMenu $database
+    fi      
+}
+
+list_data_base ()
+{
+    if [[ -d "DataBase" ]];then
+        # list all dir inside Database
+        
+        ls ./DataBase
+    else
+    
+        echo "error : DataBase file Not found please try to create database first"
+    fi
+}
+
+dbms
